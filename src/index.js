0b769fc0488771ac6a74b5f66f6878bc7a54f694
aristov <vv.aristov@gmail.com>
2017-01-23 03:22:18 +0300

    timetable

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { timetable } from './lib/timetable'
 
 const schedule = timetable({
     columns : 'Neo Fa Forte Sol Piano',
-    date : '2017-01-22',
+    // date : '2017-01-23',
     time : '09:00',
 })
 
--- src/lib/timetable.js
+++ src/lib/timetable.js
@@ -3,7 +3,9 @@ import { Grid, GridCell, rowgroup, rowheader, row, columnheader } from 'ariamodu
 import moment from 'moment'
 
 const DATE_FORMAT = 'YYYY-MM-DD'
-const DEFAULT_TIME = '09:00'
+const DEFAULT_TIME = '00:00'
+
+const { map } = Array.prototype
 
 export class TimeCell extends GridCell {}
 
@@ -14,15 +16,23 @@ export function timecell(init) {
 export class TimeTable extends Grid {
 
     init(init) {
-        const columns = init.columns.split(' ')
-        const { date = moment().format(DATE_FORMAT) } = init
-        const start = moment([date, init.time || DEFAULT_TIME].join('T'))
-        const headgroup = rowgroup({
+        super.init({ multiselectable : true })
+        if(init) {
+            const date = init.date
+            delete init.date
+            super.init(init)
+            this.date = date || moment().format(DATE_FORMAT)
+        }
+    }
+
+    set columns(columns) {
+        columns = columns.split(' ')
+        this.children = rowgroup({
             tagName : 'thead',
             children : row([
                 columnheader({
                     tabIndex : 0,
-                    children : start.format('DD/MM')
+                    children : moment().format('DD/MM')
                 }),
                 columns.map(children => columnheader({
                     style : { width : 95 / columns.length + '%' },
@@ -30,32 +40,50 @@ export class TimeTable extends Grid {
                 }))
             ])
         })
-        const bodygroup = rowgroup()
-        const day = start.day()
-        while(start.day() === day) {
-            bodygroup.children = timerow({
-                time : start,
-                children : [
-                    rowheader(start.format('HH:mm')),
-                    columns.map(() => timecell({ selected : false }))
-                ]
-            })
-            start.add(30, 'm')
-        }
-        delete init.columns
-        super.init({
-            multiselectable : true,
-            children : [headgroup, bodygroup]
-        })
-        super.init(init)
+    }
+
+    get bodies() {
+        return map.call(this.node.tBodies, ({ assembler }) => assembler)
     }
 
     set date(date) {
+        const selector = `tbody[data-date="${ date }"]`
+        const node = this.node.querySelector(selector)
+        if(node) {
+            this.bodies.forEach(body => body.hidden = body.node !== node)
+        }
+        else {
+            const columns = this.rows[0].cells.slice(1)
+            const bodygroup = rowgroup({ dataset : { date } })
+            const time = moment([date, this.time].join('T'))
+            const day = time.day()
+            while(time.day() === day) {
+                bodygroup.children = timerow({
+                    time : time,
+                    children : [
+                        rowheader(time.format('HH:mm')),
+                        columns.map(() => timecell({ selected : false }))
+                    ]
+                })
+                time.add(30, 'm')
+            }
+            this.bodies.forEach(body => body.hidden = true)
+            this.children = bodygroup
+        }
+        this.rows[0].cells[0].node.textContent = moment(date).format('DD/MM')
         this.dataset = { date }
     }
 
     get date() {
         return this.dataset.date || moment().format(DATE_FORMAT)
+    }
+
+    set time(time) {
+        this.dataset = { time }
+    }
+
+    get time() {
+        return this.dataset.time || DEFAULT_TIME
     }
 }
 
